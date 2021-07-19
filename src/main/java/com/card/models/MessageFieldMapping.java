aafd3b81ecf24b255401d696679be4cfcda7aa0d
Laukii Cheng <laukii.cheng@gmail.com>
2021-07-19 12:47:36 -0500

    fix build

--- src/main/java/com/card/models/MessageFieldMapping.java
+++ src/main/java/com/card/models/MessageFieldMapping.java
@@ -1,2 +1,10 @@
-package com.card.models;public class MessageFieldMapping {
+package com.card.models;
+
+public enum MessageFieldMapping {
+    CC_NUMBER,
+    TRANSACTION_IN_CENTS,
+    EXP_DATE,
+    RESPONSE_CODE,
+    CARD_HOLDER_NAME,
+    ZIP_CODE
 }
--- src/main/java/com/services/TransactionService.java
+++ src/main/java/com/services/TransactionService.java
@@ -10,7 +10,6 @@ public class TransactionService {
     public Transaction getTransaction(Message message) {
         return new Transaction(
                 Integer.parseInt(message.messageType),
-                Integer.parseInt(message.bitMap),
                 message.creditCardNumber,
                 LocalDate.parse(message.expirationDate),
                 Integer.parseInt(message.transactionAmountInCents),
